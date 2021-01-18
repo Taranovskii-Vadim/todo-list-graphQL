6bac79c028bcaefeaf476ada1c1ffbe41e9fa8ec
VadimTaranovscky <43213486+VadimTaranovscky@users.noreply.github.com>
2021-01-18 10:57:16 +0400

    feat(master): add express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.sendFile("/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
